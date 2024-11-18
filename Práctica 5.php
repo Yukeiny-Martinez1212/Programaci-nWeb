d34f98f6a97c1d2fbfabced4b7b696423450ca1b
Yukeiny-Martinez1212 <101664228+Yukeiny-Martinez1212@users.noreply.github.com>
2024-11-18 14:08:01 -0500

    Add files via upload

---
+++ Práctica 5.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title>
+        Practica 5
+    </title>
+</head>
+<body>
+    
+<?php
+$dia ="Miercoles";
+
+switch ($dia) {
+case "Lunes":
+Echo "Hoy es lunes";
+break;
+case "Martes":
+Echo "Hoy es martes";
+break;
+case "Miercoles":
+Echo "Hoy es miercoles";
+break;
+case "Jueves":
+Echo "Hoy es Jueves";
+break;
+case "Viernes":
+Echo "Hoy es Viernes";
+break;
+case "Sabado":
+Echo "Hoy es Sabado";
+break;
+case "Domingo":
+Echo "Hoy es Domingo";
+break;
+default:
+}
+?>
+</body>
+</html>
