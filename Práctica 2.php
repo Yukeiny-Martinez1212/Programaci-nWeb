62dfa844131091112003ad4acbba7b005186c2e5
Yukeiny-Martinez1212 <101664228+Yukeiny-Martinez1212@users.noreply.github.com>
2024-11-18 14:09:03 -0500

    Add files via upload

---
+++ Práctica 2.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+ <title>Mi primer Script en php </title>
+</head>
+<body>
+
+<?php
+
+$a=10; //Variable global
+function Variables (){
+$b=15;
+echo "esta es una prueba de funciones <br>";
+echo "La variable a es $a <br>";
+echo "La variable b es $b <br><br>";
+}
+
+echo "Esta es una prueba de variables <br>";
+echo "La variable a es $a <br>";
+echo "La variable b es $b <br>";
+
+?>
+</body>
+</html>
