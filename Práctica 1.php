c675b561c19e39370b7b40497366b86e02e93543
Yukeiny-Martinez1212 <101664228+Yukeiny-Martinez1212@users.noreply.github.com>
2024-11-18 14:09:37 -0500

    Add files via upload

---
+++ Práctica 1.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+ <title>Prácticca 1 </title>
+</head>
+<body>
+
+<?php
+ECHO "Hola! este es mi primer script en PHP  <BR>";
+
+//Esto es una line de comentario
+
+#Esto también es una linea de comentario
+
+/*Esto es un
+bloque de más de una linea de comentarios*/
+
+$vartex="Hola! estoy usando vaiables para guardar texto <BR>"; 
+echo $vartex;
+$varnum1=5;
+$varnum2=6;
+$res=$varnum1+$varnum2;
+echo "El resultado de la suma de $varnum1 y $varnum2 es =$res <BR>"; 
+?>
+</body>
+</html>
