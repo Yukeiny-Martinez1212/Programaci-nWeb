989960670426e35da48480e9265411ec3f974d76
Yukeiny-Martinez1212 <101664228+Yukeiny-Martinez1212@users.noreply.github.com>
2024-11-18 14:08:25 -0500

    Add files via upload

---
+++ Práctica 4.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>
+        Practica 4
+    </title>
+</head>
+<body>
+    
+
+<?php
+$numero =10;
+
+//Verificar si el numero es par o impar
+if ($numero % 2 ==0){
+echo "El numero $numero es par.";
+} else {
+echo "El numero $numero es impar.";
+}
+?>
+</body>
+</html>
