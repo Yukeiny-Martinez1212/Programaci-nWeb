026cd9b127bca76337f7b2565aa44999f8449c1f
Yukeiny-Martinez1212 <101664228+Yukeiny-Martinez1212@users.noreply.github.com>
2024-12-09 15:58:57 -0500

    Add files via upload

---
+++ PRACTICA11.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title> Practica 11 </title>
+</head>
+<body>
+
+<?php
+require ("config.php");
+echo "<h1>Creacion de una tabla con variables</h1><br><br>";
+$conexion=mysqli_connect ($servidor, $usuario, $password, $bd);
+$tabla="Usuarios";
+
+$campo1="id";
+$campotipo1="int";
+$long1="4";
+
+$campo2="nombre";
+$campotipo2="varchar";
+$long2="50";
+
+$campo3="email";
+$campotipo3="varchar";
+$long3="70";
+
+$campo4="edad";
+$campotipo4="int";
+$long4="3";
+
+$consulta="create table $tabla ($campo1 $campotipo1 auto_increment primary key, $campo2 $campotipo2($long2), $campo3 $campotipo3($long3), $campo4 $campotipo4($long4))";
+if ($resultado=mysqli_query ($conexion, $consulta))
+{
+    echo "La tabla $tabla, se creo con exito <br>";
+    echo "El script utilizado fue $consulta <br><br>";
+}
+else
+{
+    echo "La tabla $tabla no se ha podido crear <br>".mysqli_error($conexion);
+}
+echo "<br><br>";
+mysqli_close($conexion)
+?>
+</body>
+</html>
+
+
