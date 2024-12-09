dc2e82bc4f8d6d0a5941a8af098147eb29c6eb6c
Yukeiny-Martinez1212 <101664228+Yukeiny-Martinez1212@users.noreply.github.com>
2024-12-09 16:09:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,22 @@
 <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/PRACTICA3.HTML" title= "Letras que te llevan a Practica 3"> Practia No.3 </a>
  <br>
 	  <a href="https://yukeiny-martinez1212.github.io/Programaci-Web/PracticaX4.html" title= "Letras que te llevan a Practica 4"> Pracitca No.4 </a>
+	  <br>
      <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/PracticaCinco.html" title= "Letras que te llevan a Practica 5"> Practica No.5 </a>
 	<br>
 	  <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/Practica6.html" title= "Letras que te llevan a Practica 6"> Practica No.6 </a>
+	  <H1> PRACTICAS PHP </H1>
+	   <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/Práctica 1.php" title= "Letras que te llevan a Practica 1"> Practica No.1 PHP </a>   <br>
+	      <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/Práctica 2.php" title= "Letras que te llevan a Practica 2"> Practica No.2 PHP </a>   <br>
+	     <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/Practica 3 (1).php" title= "Letras que te llevan a Practica 3"> Practica No.3 PHP </a>   <br>
+	     <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/Práctica 4.php" title= "Letras que te llevan a Practica 4"> Practica No.4 PHP </a>   <br> 
+	     <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/Práctica 5.php" title= "Letras que te llevan a Practica 5"> Practica No.5 PHP </a>   <br>
+	     <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/Practica 6.php" title= "Letras que te llevan a Practica 6"> Practica No.6 PHP </a>   <br>
+	     <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/Practica 7.php" title= "Letras que te llevan a Practica 7"> Practica No.7 PHP </a>   <br>
+	     <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/PRACTICA8.php" title= "Letras que te llevan a Practica 8"> Practica No.8 PHP </a>   <br>
+	     <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/PRACTICA9.php" title= "Letras que te llevan a Practica 9"> Practica No.9 PHP </a>   <br>
+	   <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/PRACTICA10.php" title= "Letras que te llevan a Practica 9"> Practica No.10 PHP </a>   <br>
+	   <a href="https://yukeiny-martinez1212.github.io/Programaci-nWeb/PRACTICA11.php" title= "Letras que te llevan a Practica 9"> Practica No.11 PHP </a>   <br>
 	
   </body>
   </html>
